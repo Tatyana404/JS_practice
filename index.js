7e46a2dc9471d5d00111a0a86b12becfc73fe548
Tatyana404 <kot.kate2020@gmail.com>
2020-12-31 00:34:24 +0200

    Add find the minimum and maximum number

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
 "use strict";
 
+// Найдите минимальное и максимальное число
+
+function getMinAndMaxNumbers() {
+  return [getMin(arguments), getMax(arguments)];
+}
+
+function getMin(arr) {
+  let min = arr[0];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+  }
+  return min;
+}
+function getMax(arr) {
+  let max = arr[0];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+  }
+  return max;
+}
